15142a9ec9cfb333fbf196ae7b8b9304d29b38ce
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-17 12:13:09 -0300

    Bônus: animação de CSS no Hover do Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <div className="card">
+        <h1>Título do item</h1>
+        <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
+      </div>
+    </>
+  );
 }
 
 export default App;
