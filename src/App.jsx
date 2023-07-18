756e96ae1cf0a89a88e044e50a307a7cffe17bb2
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-18 10:23:40 -0300

    Criação do componente `Card` (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <h1>Título do item</h1>
         <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
       </div>
+      <Card />
     </>
   );
 }
