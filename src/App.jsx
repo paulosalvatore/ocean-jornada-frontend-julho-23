1b6162d1d89af7e8c936fbd3779287254c02627f
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-18 11:42:40 -0300

    Extração do componente `Card`

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Card from "./components/Card/Card";
 function App() {
   return (
     <>
-      <div className="card">
-        <h1>Título do item</h1>
-        <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
-      </div>
+      <Card />
+      <Card />
       <Card />
     </>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,10 @@
+import "./Card.css";
+
 export default function Card() {
-  return <>Teste do Card</>;
+  return (
+    <div className="card">
+      <h1>Título do item</h1>
+      <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
+    </div>
+  );
 }
