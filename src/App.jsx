fbbe25f983010a1a19d978c374fdac8538a66e54
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-19 10:24:55 -0300

    `export` removido dos itens no `App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import Card from "./components/Card/Card";
 
-export const item1 = {
+const item1 = {
   nome: "Rick Sanchez",
   imagem:
     "https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png",
 };
 
-export const item2 = {
+const item2 = {
   nome: "Morty Smith",
   imagem:
     "https://comicvine.gamespot.com/a/uploads/scale_medium/6/66303/4469088-tumblr_inline_n0aleph3fl1r8rr6o.jpg",
